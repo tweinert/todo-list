5a081950375b3b10cbe71b713ac443d916b43337
tweinert <tait.weinert@outlook.com>
2022-03-27 13:47:23 +1030

    basic setup

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("this should be visible");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwidGhpcyBzaG91bGQgYmUgdmlzaWJsZVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
